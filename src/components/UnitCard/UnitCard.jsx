90d3be9595f3d20257d539c8262f097c0f4c65fb
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2021-02-20 19:36:25 +0200

    Added unit card images for all Beastmen units.

--- src/components/UnitCard/UnitCard.jsx
+++ src/components/UnitCard/UnitCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UnitCard = ({ unitName, unitCategory, unitPrice, unitImage }) => {
   return (
     <img
-      src={require(`../../images/bst_ch_malagor_0.png`).default}
+      src={require(`../../images/${unitImage}.png`).default}
       alt={unitName}
       title={unitName}
       width="60"
