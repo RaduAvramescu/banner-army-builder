2e16529e2845c6d053edea8674cadb5e4016d31c
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2021-03-01 16:37:07 +0200

    Updated JSON with preliminary spells data; added and updated tabs for mounts, spells, and abilities.

--- src/components/UnitDialog/UnitDialog.jsx
+++ src/components/UnitDialog/UnitDialog.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -6,9 +8,47 @@ import ListItemText from "@material-ui/core/ListItemText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import PersonIcon from "@material-ui/icons/Person";
+
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 function SimpleDialog(props) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const { onClose, open } = props;
 
   const handleListItemClick = (value) => {
@@ -17,29 +57,86 @@ function SimpleDialog(props) {
 
   return (
     <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">Select a mount.</DialogTitle>
-      <List>
-        <ListItem button onClick={() => handleListItemClick(null)}>
-          <ListItemText primary="None" />
-        </ListItem>
-        {props.hasOwnProperty("mounts") &&
-          props.mounts.map((el, i) => (
-            <ListItem button onClick={() => handleListItemClick(el)} key={i}>
-              <ListItemAvatar>
-                <Avatar
-                  alt={el.name}
-                  src={
-                    require(`../../images/unit_card/card_icon/${el.image}.png`)
-                      .default
-                  }
-                >
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={el.name} />
+      <div>
+        <AppBar position="static" color="transparent">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="Lord/Hero Tabs"
+          >
+            <Tab label="Mounts" {...a11yProps(0)} />
+            <Tab label="Spells" {...a11yProps(1)} />
+            <Tab label="Abilities" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <List>
+            <ListItem button onClick={() => handleListItemClick(null)}>
+              <ListItemAvatar></ListItemAvatar>
+              <ListItemText primary="None" />
             </ListItem>
-          ))}
-      </List>
+            {props.hasOwnProperty("mounts") &&
+              props.mounts.map((el, i) => (
+                <ListItem
+                  button
+                  onClick={() => handleListItemClick(el)}
+                  key={i}
+                >
+                  <ListItemAvatar>
+                    <Avatar
+                      alt={el.name}
+                      src={
+                        require(`../../images/unit_card/card_icon/${el.image}.png`)
+                          .default
+                      }
+                    >
+                      <PersonIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={el.name} />
+                </ListItem>
+              ))}
+          </List>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <List>
+            <ListItem button onClick={() => handleListItemClick(null)}>
+              <ListItemAvatar></ListItemAvatar>
+              <ListItemText primary="None" />
+            </ListItem>
+            {props.hasOwnProperty("spells") &&
+              props.spells.map((el, i) => (
+                <ListItem
+                  button
+                  onClick={() => handleListItemClick(el)}
+                  key={i}
+                >
+                  <ListItemAvatar></ListItemAvatar>
+                  <ListItemText primary={el.name} />
+                </ListItem>
+              ))}
+          </List>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <List>
+            <ListItem button onClick={() => handleListItemClick(null)}>
+              <ListItemAvatar></ListItemAvatar>
+              <ListItemText primary="None" />
+            </ListItem>
+            {props.hasOwnProperty("abilities") &&
+              props.abilities.map((el, i) => (
+                <ListItem
+                  button
+                  onClick={() => handleListItemClick(el)}
+                  key={i}
+                >
+                  <ListItemAvatar></ListItemAvatar>
+                  <ListItemText primary={el.name} />
+                </ListItem>
+              ))}
+          </List>
+        </TabPanel>
+      </div>
     </Dialog>
   );
 }
