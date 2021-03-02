9b28c78cd42aa784d9cfa18790bac95306c7ff96
RaduAvramescu <71271545+RaduAvramescu@users.noreply.github.com>
2021-03-02 12:21:31 +0200

    Separated roster categories into categories.json; refactored FactionRoster.jsx.

--- src/components/FactionRoster/FactionRoster.jsx
+++ src/components/FactionRoster/FactionRoster.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { Box, Grid, Typography, CircularProgress } from "@material-ui/core";
+import categories from "../../data/categories.json";
+import UnitCard from "../UnitCard/UnitCard";
+
 import Beastmen from "../../data/Beastmen.json";
 import Bretonnia from "../../data/Bretonnia.json";
 import DarkElves from "../../data/Dark Elves.json";
@@ -15,40 +19,38 @@ import VampireCoast from "../../data/Vampire Coast.json";
 import VampireCounts from "../../data/Vampire Counts.json";
 import WarriorsOfChaos from "../../data/Warriors of Chaos.json";
 import WoodElves from "../../data/Wood Elves.json";
-import UnitCard from "../UnitCard/UnitCard";
-import { Box, Grid, Typography } from "@material-ui/core";
 
-const categories = [
-  { name: "Lords", value: "Lords" },
-  { name: "Heroes", value: "Hero" },
-  { name: "Infantry", value: "Infantry" },
-  { name: "Missile Infantry", value: "Missile Infantry" },
-  { name: "Cavalry & Chariots", value: "Cavalry & Chariots" },
-  { name: "Missile Cavalry & Chariots", value: "Missile Cavalry & Chariots" },
-  { name: "Monsters & Beasts", value: "Monsters & Beasts" },
-  { name: "Missile Monsters & Beasts", value: "Missile Monsters & Beasts" },
-  { name: "Artillery & War Machines", value: "Artillery & War Machines" },
+const factions = [
+  Beastmen,
+  Bretonnia,
+  DarkElves,
+  Dwarfs,
+  Empire,
+  Greenskins,
+  HighElves,
+  Lizardmen,
+  Norsca,
+  Skaven,
+  TombKings,
+  VampireCoast,
+  VampireCounts,
+  WarriorsOfChaos,
+  WoodElves,
 ];
 
 const FactionRoster = ({ selectedFaction, onUnitAdd }) => {
-  const factions = [
-    Beastmen,
-    Bretonnia,
-    DarkElves,
-    Dwarfs,
-    Empire,
-    Greenskins,
-    HighElves,
-    Lizardmen,
-    Norsca,
-    Skaven,
-    TombKings,
-    VampireCoast,
-    VampireCounts,
-    WarriorsOfChaos,
-    WoodElves,
-  ];
   let factionRoster = factions[selectedFaction];
+
+  if (
+    typeof categories == undefined ||
+    typeof selectedFaction == undefined ||
+    typeof factionRoster == undefined
+  )
+    return (
+      <Box display="flex" justifyContent="center" my="1rem">
+        <CircularProgress />
+      </Box>
+    );
 
   return (
     <Box my="1rem">
@@ -58,34 +60,28 @@ const FactionRoster = ({ selectedFaction, onUnitAdd }) => {
         </Typography>
       </Box>
       <Grid container justify="center" alignContent="center" direction="column">
-        {categories && (
-          <div>
-            {categories.map((caste, i) => (
-              <div key={i} id={i}>
-                <Box my="1rem">
-                  <Typography variant="h3" component="h2" align="center">
-                    {categories[i].name}
-                  </Typography>
-                  <Grid container justify="center">
-                    {selectedFaction &&
-                      factionRoster &&
-                      factionRoster
-                        .filter((unit) => unit.caste === categories[i].value)
-                        .map((unit, i) => (
-                          <UnitCard
-                            key={i}
-                            id={i}
-                            onUnitAdd={onUnitAdd}
-                            addUnit={true}
-                            {...unit}
-                          />
-                        ))}
-                  </Grid>
-                </Box>
-              </div>
-            ))}
-          </div>
-        )}
+        <div>
+          {categories.map((caste, i) => (
+            <Box key={i} id={i} my="1rem">
+              <Typography variant="h3" component="h2" align="center">
+                {categories[i].name}
+              </Typography>
+              <Grid container justify="center">
+                {factionRoster
+                  .filter((unit) => unit.caste === categories[i].value)
+                  .map((unit, i) => (
+                    <UnitCard
+                      key={i}
+                      id={i}
+                      onUnitAdd={onUnitAdd}
+                      addUnit={true}
+                      {...unit}
+                    />
+                  ))}
+              </Grid>
+            </Box>
+          ))}
+        </div>
       </Grid>
     </Box>
   );
