005d64e1c09b7a03a60bca62251f8e5055576304
Radu Avramescu <71271545+RaduAvramescu@users.noreply.github.com>
2021-04-20 17:21:15 +0300

    Separated SimpleDialog styling into a separate file.

--- src/components/SimpleDialog/SimpleDialog.view.jsx
+++ src/components/SimpleDialog/SimpleDialog.view.jsx
@@ -7,34 +7,7 @@ import {
   DialogTitle,
   DialogContent,
 } from "@material-ui/core";
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  btn__container: {
-    position: "absolute",
-    right: theme.spacing(0.5),
-    top: theme.spacing(0.5),
-  },
-  btn: {
-    color: "#a09d9a",
-    background: `url("images/ui/skins/default/button_basic.png") no-repeat`,
-    backgroundPosition: "-0px -0px",
-    height: "40px",
-    width: "40px",
-    lineHeight: "40px",
-    "&:hover": {
-      backgroundPosition: "-0px -40px",
-      color: "#fff",
-    },
-  },
-  dialog: {
-    border: "1px solid transparent",
-    borderImage: `url("images/ui/skins/default/panel_back_border.png") 30 / 30px / 7px round`,
-  },
-});
+import styles from "./SimpleDialog.styles";
 
 const SimpleDialog = withStyles(styles)((props) => {
   const { children, classes, onClose, open, ...other } = props;
